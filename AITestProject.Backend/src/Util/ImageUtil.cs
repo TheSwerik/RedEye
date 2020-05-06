e5d1d41f9cc52ba59eeb4721cd32ed53e939ce46
TheSwerik <derswerik@gmail.com>
2020-05-06 11:09:37 +0200

    cleanup

--- AITestProject.Backend/src/Util/ImageUtil.cs
+++ AITestProject.Backend/src/Util/ImageUtil.cs
@@ -4,13 +4,12 @@ using System.Linq;
 
 namespace AITestProject.Util
 {
-    public class ImageUtil
+    public static class ImageUtil
     {
         public static IEnumerable<string> GetImages(string folder)
         {
-            return Directory
-                .GetFiles(folder, "*.*", SearchOption.AllDirectories)
-                .Where(Path.HasExtension);
+            return Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories)
+                            .Where(Path.HasExtension);
         }
     }
 }
